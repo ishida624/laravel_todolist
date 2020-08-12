3f12b23ab0e247473007d4ac3c54e8c9d2315077
Gill <ishida624@gmail.com>
2020-08-12 16:29:43 +0800

    update admin migration class name

--- database/migrations/2020_08_06_131418_admin.php
+++ database/migrations/2020_08_06_131418_admin.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Admin2 extends Migration
+class Admin extends Migration
 {
     /**
      * Run the migrations.
